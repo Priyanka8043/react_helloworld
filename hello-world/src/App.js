6f400f933997373c87a097ea59cb38271da9aa5b
Priyanka <priyankaspatil610@gmail.com>
2024-05-05 15:31:05 +0530

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <center>
+      <h1>Hello, World!</h1>
+      </center>
+      
+    </div>
+  );
+}
+
+export default App;
+
+// npx create-react-app hello-react
+//  cd hello-world
+//npm start
